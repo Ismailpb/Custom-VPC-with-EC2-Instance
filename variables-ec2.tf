83897cd161df99f0aca0cc41321a45785b105e63
Ismailpb <94457544+Ismailpb@users.noreply.github.com>
2021-12-07 10:27:37 +0530

    Add files via upload

---
+++ variables-ec2.tf
@@ -0,0 +1,7 @@
+variable "ami" {
+  default = "ami-002068ed284fb165b"  
+}
+
+variable "type" {
+  default = "t2.micro"
+} 
---
+++ variables-vpc.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "project" {
+  default = "Supportteam"
+}
+
+variable "vpc_cidr" { 
+  default = "172.17.0.0/16"
+}
+
